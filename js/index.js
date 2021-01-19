67d4404eb069bf118c7a345b622f680ef96c70cd
Georgina Morales <georginamp87@gmail.com>
2021-01-19 23:44:20 +0100

    changed my mind I want to stop time as superpower

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+let price = product.querySelector('.price span')
+let quantity = product.querySelector('.quantity input')
+let subtotal = product.querySelector('.subtotal span')
+let newSubtotal = Number(price.innerText) * (quantity.value)
+subtotal = subtotal.innerText = newSubtotal
+ return newSubtotal
 
   //... your code goes here
 }
@@ -9,34 +15,43 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let sum = 0
+  let allProducts = document.querySelectorAll('.product')
+ for (let i=0 ; i < allProducts.length ; i ++) {
+    sum += updateSubtotal(allProducts[i])
+} 
 
-  // ITERATION 3
-  //... your code goes here
+  //  ITERATION 3
+  let totalValue = document.querySelector('#total-value span')
+ totalValue.innerText = sum
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  // const target = event.currentTarget;
+  // console.log('The target in remove is:', target);
+  // let currentTarget = document.getElementsByTagName('tbody')
+  // currentTarget.removeChild('.product')
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  // let newProduct = document.createElement('tr')
+  // tbody.appendChild(newProduct)
+  // /... your code goes here
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtn = document.getElementsByClassName('btn btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
 });
